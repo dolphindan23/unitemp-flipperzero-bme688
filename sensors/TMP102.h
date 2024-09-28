93eeb37a1d164c117af6c23e2abd568922acbc2b
dolphindan23 <150243343+dolphindan23@users.noreply.github.com>
2024-09-28 09:27:41 -0700

    Update TMP102.h

--- sensors/TMP102.h
+++ sensors/TMP102.h
@@ -1,1 +1,16 @@
+#ifndef TMP102_H
+#define TMP102_H
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "Sensor.h"
+
+extern const SensorType TMP102;
+
+bool unitemp_TMP102_I2C_alloc(Sensor* sensor, char* args);
+bool unitemp_TMP102_I2C_free(Sensor* sensor);
+bool unitemp_TMP102_init(Sensor* sensor);
+bool unitemp_TMP102_I2C_deinit(Sensor* sensor);
+UnitempStatus unitemp_TMP102_I2C_update(Sensor* sensor);
+
+#endif // TMP102_H
